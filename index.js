e21b5276c96eec8fe47691fe738a097d5388ccd5
deep005 <guhadeepshanker@gmail.com>
2017-10-20 22:44:15 +0530

    basic chat functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let express = require('express');
+let socket = require('socket.io');
 
 
 let app = express();
@@ -10,4 +11,13 @@ let server = app.listen(4000, ()=>{
 // static files
 app.use(express.static('public'));
 
+// socket setup
+let io = socket(server);
 
+io.on('connection', function(socket){
+       console.log('made socket connection', socket.id);
+
+       socket.on('chat', function(data){
+              io.sockets.emit('chat',data);
+       })
+});
